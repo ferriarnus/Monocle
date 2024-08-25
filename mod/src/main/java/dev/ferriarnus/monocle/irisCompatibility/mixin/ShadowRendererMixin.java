fb2c4f8b893981070d7d300297ce966aa2588496
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-08-25 20:12:20 +0200

    Update ShadowRendererMixin.java

--- mod/src/main/java/dev/ferriarnus/monocle/irisCompatibility/mixin/ShadowRendererMixin.java
+++ mod/src/main/java/dev/ferriarnus/monocle/irisCompatibility/mixin/ShadowRendererMixin.java
@@ -1,5 +1,7 @@
 package dev.ferriarnus.monocle.irisCompatibility.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.ferriarnus.monocle.irisCompatibility.impl.BoxCullingFrustum;
 import dev.ferriarnus.monocle.irisCompatibility.impl.CullEverythingFrustum;
@@ -11,12 +13,11 @@ import net.irisshaders.iris.shadows.frustum.BoxCuller;
 import net.irisshaders.iris.shadows.frustum.advanced.AdvancedShadowCullingFrustum;
 import net.irisshaders.iris.uniforms.CapturedRenderingState;
 import net.minecraft.client.renderer.culling.Frustum;
-import org.joml.Matrix4f;
-import org.joml.Matrix4fc;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
@@ -29,20 +30,36 @@ public class ShadowRendererMixin {
     @Final
     @Shadow
     private float halfPlaneLength;
+    @Unique
+    private Vector3f shadowLightVectorFromOrigin;
+    @Unique
+    private BoxCuller box;
 
     @ModifyArg(method = "createShadowFrustum", at = @At(ordinal = 0, value = "INVOKE", target = "Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;setInfo(Lnet/minecraft/client/renderer/culling/Frustum;Ljava/lang/String;Ljava/lang/String;)Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;"))
     private Frustum changeEverythingCuller(Frustum frustum) {
         return new CullEverythingFrustum();
     }
 
+    @WrapOperation(method = "createShadowFrustum", at = @At(value = "INVOKE", target = "Lorg/joml/Vector3f;normalize()Lorg/joml/Vector3f;"))
+    private Vector3f captureVector(Vector3f instance, Operation<Vector3f> original) {
+        this.shadowLightVectorFromOrigin = instance;
+        return original.call(instance);
+    }
+
+    @WrapOperation(method = "createShadowFrustum", at = @At(value = "NEW", target = "(D)Lnet/irisshaders/iris/shadows/frustum/BoxCuller;"))
+    private BoxCuller captureBox(double maxDistance, Operation<BoxCuller> original) {
+        this.box = original.call(maxDistance);
+        return this.box;
+    }
+
     @ModifyArg(method = "createShadowFrustum", at = @At(ordinal = 1, value = "INVOKE", target = "Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;setInfo(Lnet/minecraft/client/renderer/culling/Frustum;Ljava/lang/String;Ljava/lang/String;)Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;"))
-    private Frustum changeReverseCuller(Frustum frustum, @Local(name = "shadowLightVectorFromOrigin") Vector3f shadowLightVectorFromOrigin, @Local BoxCuller boxCuller, @Local(argsOnly = true) float renderMultiplier) {
+    private Frustum changeReverseCuller(Frustum frustum, @Local(argsOnly = true) float renderMultiplier, @Local BoxCuller boxCuller) {
         return new ReversedAdvancedShadowCullingFrustum(CapturedRenderingState.INSTANCE.getGbufferModelView(), this.shouldRenderDH && DHCompat.hasRenderingEnabled() ? DHCompat.getProjection() : CapturedRenderingState.INSTANCE.getGbufferProjection(), shadowLightVectorFromOrigin, boxCuller, new BoxCuller(this.halfPlaneLength * renderMultiplier));
     }
 
     @ModifyArg(method = "createShadowFrustum", at = @At(ordinal = 2, value = "INVOKE", target = "Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;setInfo(Lnet/minecraft/client/renderer/culling/Frustum;Ljava/lang/String;Ljava/lang/String;)Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;"))
-    private Frustum changeAdvShadowCuller(Frustum frustum, @Local(name = "shadowLightVectorFromOrigin") Vector3f shadowLightVectorFromOrigin, @Local BoxCuller boxCuller) {
-        return new AdvancedShadowCullingFrustum(CapturedRenderingState.INSTANCE.getGbufferModelView(), this.shouldRenderDH && DHCompat.hasRenderingEnabled() ? DHCompat.getProjection() : CapturedRenderingState.INSTANCE.getGbufferProjection(), shadowLightVectorFromOrigin, boxCuller);
+    private Frustum changeAdvShadowCuller(Frustum frustum) {
+        return new AdvancedShadowCullingFrustum(CapturedRenderingState.INSTANCE.getGbufferModelView(), this.shouldRenderDH && DHCompat.hasRenderingEnabled() ? DHCompat.getProjection() : CapturedRenderingState.INSTANCE.getGbufferProjection(), shadowLightVectorFromOrigin, box);
     }
 
     @ModifyArg(method = "createShadowFrustum", at = @At(ordinal = 3, value = "INVOKE", target = "Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;setInfo(Lnet/minecraft/client/renderer/culling/Frustum;Ljava/lang/String;Ljava/lang/String;)Lnet/irisshaders/iris/shadows/frustum/FrustumHolder;"))
